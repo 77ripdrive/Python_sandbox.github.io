e81b04d5d4af7e31461b69c1e03bb4c8dcd60d31
GT <77ripdrive@gmail.com>
2021-01-03 14:25:39 +0300

    fix for jsonschema

--- tests/test_json_schema_base.py
+++ tests/test_json_schema_base.py
@@ -3,6 +3,7 @@ import json
 
 import pytest
 from jsonschema import Draft7Validator
+from jsonschema import validate
 
 path_to_test_data_csv_file = "../asset/data_file.csv"
 path_json_schema_file = "../asset/schema_first.json"
@@ -23,25 +24,26 @@ def get_data_from_json_schema(path):
         return json.load(schema_file)
 
 
-payload = {"productId": 1, "productName": "A green door", "price": 12.50, "tags": ["home", "green"]}
+payload = {"productId": 1, "productName": "A green door", "productPrice": 12.50, "tags": ["home", "green"]}
 
 
 def test_using_csv_with_different_fields():
-    with open("../asset/schema_first.json", "r") as schema_file:
+    with open(path_json_schema_file, "r") as schema_file:
         schema = json.load(schema_file)
     result = Draft7Validator(schema).is_valid(payload)
     print(result)
     assert result == True
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 @pytest.mark.parametrize("productId,productName,price,tags", read_test_data_from_csv(path_to_test_data_csv_file))
 def test_using_csv_with_different_fields_negative(productId, productName, price, tags):
     schema = get_data_from_json_schema(path_json_schema_file)
     payload.update({"productId": productId})
     payload.update({"productName": productName})
-    payload.update({"price": price})
+    payload.update({"productPrice": price})
     payload.update({"tags": tags})
-    print(payload)
-    result = Draft7Validator(schema).is_valid(payload)
-    assert result == True
+    try:
+        validate(payload, schema)
+    except:
+        print(f" -  Validation error")
